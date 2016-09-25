9947d2ea5e3f3646a879f402f157339c60eff248
Hugh <hugh_gleaves@hotmail.com>
2016-09-25 10:20:38 -0700

    Tweaks.

--- Assemblify/Program.cs
+++ Assemblify/Program.cs
@@ -8,10 +8,16 @@ namespace TestAssemblify
         {
             string assembly_folder = @"E:\assemblify";
 
+            // Create an Assemblify AssemblyFile object from a DLL's path:
+
             var a = AssemblyFile.Create(@"E:\Git\Repos\Github\assemblify\Assemblify.Core\bin\Debug\Assemblify.Core.dll");
+
+            // If this assembly has not yet been published into the designated folder, then publish it.
 
             if (a.IsPublished(assembly_folder) == false)
                 a.Publish(assembly_folder);
+
+            // Did the whole process actually work?
 
             bool p = a.IsPublished(assembly_folder);
         }
