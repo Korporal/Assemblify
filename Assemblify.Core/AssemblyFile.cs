342f659a1cfe42e88c6cbf6496fd63784729a6d5
Hugh <hugh_gleaves@hotmail.com>
2016-09-25 09:58:09 -0700

    Cleanup 'using' directives.

--- Assemblify.Core/AssemblyFile.cs
+++ Assemblify.Core/AssemblyFile.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Versioning;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assemblify.Core
 {
