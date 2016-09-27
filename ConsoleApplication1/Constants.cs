e67f0a123dfe0934f7030de0daa8497091812173
Hugh <hugh_gleaves@hotmail.com>
2016-09-26 20:12:54 -0700

    Test app so we can debug the VSIX as it uses a real solution.

---
+++ ConsoleApplication1/Constants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Constants
+    {
+        public static class Calendar
+        {
+            public const int HOURS_PER_DAY = 24;
+            public const int MINS_PER_HOUR = 60;
+            public const int SECS_PER_MIN = 60;
+        }
+
+        public static class BreakdownVoltage
+        {
+            public const double Air = 3.0;
+            public const double Alumina = 13.4;
+            public const double WindowGlass = 9.8;
+            public const double Borsosilicate = 20;
+            public const double MineralOil = 10;
+            public const double Benzene = 163;
+            public const double Polystyrene = 19.7;
+            public const double Polyethylene = 19;
+        }
+
+    }
+}
