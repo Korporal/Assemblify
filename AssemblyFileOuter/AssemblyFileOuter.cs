08b2bf8236edcae34a9f5870446b29024b9e3021
Hugh Gleaves <hgleaves@ncu.edu>
2016-09-29 14:37:17 -0700

    TEST - Establish a dependency between the test outer and inner classes - for testing.

--- AssemblyFileOuter/AssemblyFileOuter.cs
+++ AssemblyFileOuter/AssemblyFileOuter.cs
@@ -8,5 +8,10 @@ namespace AssemblyFileOuter
 {
     public class AssemblyFileOuter
     {
+        private AssemblyFileInner.AssemblyFileInner blob;
+        public AssemblyFileOuter()
+        {
+            blob = new AssemblyFileInner.AssemblyFileInner();
+        }
     }
 }
