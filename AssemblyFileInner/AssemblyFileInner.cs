bf6892165286892e31cad380393702c53b145975
Hugh <hugh_gleaves@hotmail.com>
2016-10-08 12:16:01 -0700

    TEST - Minor changes to establish a true dependency on an external MS assembly (System.Numerics).

--- AssemblyFileInner/AssemblyFileInner.cs
+++ AssemblyFileInner/AssemblyFileInner.cs
@@ -1,10 +1,16 @@
-﻿namespace AssemblyFileInner
+﻿using System.Numerics;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AssemblyFileInner
 {
     public class AssemblyFileInner
     {
+        BinaryFormatter formatter; // NOTE: This class acutally sits inside mscorlib !!
+        Complex complex;
         public AssemblyFileInner()
         {
-            ;
+            formatter = new BinaryFormatter();
+            complex = new Complex(12, 6);
         }
     }
 }
